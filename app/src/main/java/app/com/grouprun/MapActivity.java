d88f134dd1708f04a070968576fb33f4b53e5ad1
Luseniik <lkromah@aggies.ncat.edu>
2016-02-10 22:39:47 -0500

    Updates
    Added Drawer Layout
    Added activity for list of runs

--- app/src/main/java/app/com/grouprun/MapActivity.java
+++ app/src/main/java/app/com/grouprun/MapActivity.java
@@ -1,7 +1,10 @@
 package app.com.grouprun;
+
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -11,13 +14,16 @@ import android.os.Build;
 import android.os.SystemClock;
 import android.speech.tts.TextToSpeech;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Chronometer;
@@ -43,10 +49,14 @@ import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
 import com.pubnub.api.PubnubError;
 import com.pubnub.api.PubnubException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.Locale;
+
 import info.hoang8f.widget.FButton;
+
 public class MapActivity extends AppCompatActivity implements
         OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
@@ -149,12 +159,21 @@ public class MapActivity extends AppCompatActivity implements
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     /*
         Where all ui components are handled
      */
-    public void uiComponents(){
+    public void uiComponents() {
         button = (FButton) findViewById(R.id.startButton);
         chronometer = (Chronometer) findViewById(R.id.chronometer);
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
@@ -272,8 +291,20 @@ public class MapActivity extends AppCompatActivity implements
     @Override
     public void onConnected(Bundle connectionHint) {
         LocationRequest mLocationRequest = createLocationRequest();
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
-                mLocationRequest, this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+            return;
+        }
+
+
     }
 
     private LocationRequest createLocationRequest() {
@@ -344,15 +375,24 @@ public class MapActivity extends AppCompatActivity implements
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_share) {
+        if (id == R.id.home) {
+            Intent mapViewIntent = new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(mapViewIntent);
+        } else if (id == R.id.run_history) {
+            Intent runViewIntent = new Intent(getApplicationContext(), RunListActivity.class);
+            startActivity(runViewIntent);
+        }else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
@@ -402,4 +442,6 @@ public class MapActivity extends AppCompatActivity implements
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
     }
+
+
 }
