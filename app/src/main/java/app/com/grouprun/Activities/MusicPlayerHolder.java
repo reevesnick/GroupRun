17d5e62f9ef90a83c815114c607037c95e97f538
Neegbeah Reeves <Nehemiahsal@aol.com>
2016-02-27 21:45:40 -0500

    Checkpoint #2

--- app/src/main/java/app/com/grouprun/Activities/MusicPlayerHolder.java
+++ app/src/main/java/app/com/grouprun/Activities/MusicPlayerHolder.java
@@ -3,5 +3,27 @@ package app.com.grouprun.Activities;
 /**
  * Created by neegbeahreeves on 2/22/16.
  */
-public class MusicPlayerHolder {
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import app.com.grouprun.R;
+
+/**
+ * This is only the UI of the music player holder. It can be deleted at any time.
+ *
+ */
+public class MusicPlayerHolder extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+
 }
