effbd9f568e9a91485f916e65cdf174ae6b1e6ba
Neegbeah Reeves <Nehemiahsal@aol.com>
2016-02-28 13:53:12 -0500

    Commit Checkpoint #3

--- app/src/main/java/app/com/grouprun/Activities/MapActivity.java
+++ app/src/main/java/app/com/grouprun/Activities/MapActivity.java
@@ -401,42 +401,18 @@ public class MapActivity extends AppCompatActivity implements
 
 
 
-    private double distance(double lat1, double lon1, double lat2, double lon2) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        return (dist);
-
+    public double getDistance(double lat1, double lon1, double lat2, double lon2) {
+        double latA = Math.toRadians(lat1);
+        double lonA = Math.toRadians(lon1);
+        double latB = Math.toRadians(lat2);
+        double lonB = Math.toRadians(lon2);
+        double cosAng = (Math.cos(latA) * Math.cos(latB) * Math.cos(lonB-lonA)) +
+                (Math.sin(latA) * Math.sin(latB));
+        double ang = Math.acos(cosAng);
+        double dist = ang *6371;
+        return dist;
     }
 
-    private double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
-    private double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
-    }
-/*
-    public void distanceBetween(){
-        double startLatitude = 0;
-        double startLongitude = 0;
-        double endLatitude = 0;
-        double endLongitude = 0;
-
-
-        double R, a, c, d, dLat,dLon;
-        R=6371.00;
-        dLat = (endLatitude - startLatitude);
-        Math.toRadians(dLat);
-        dLon = (endLongitude - startLongitude);
-        Math.toRadians(dLon);
-        a = Math.sin(dLat/2.00) * Math.sin(dLat/2.00) + Math.cos(Math.toRadians(startLatitude)) * Math.cos(Math.toRadians(endLatitude)) * Math.sin(dLon/2) * Math.sin(dLat/2);
-        c =2 * Math.atan2(Math.sqrt(a), Math.sqrt(1.00-a));
-        d= R * c;
-        System.out.println("Miles: "+d);
-    }
-*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
